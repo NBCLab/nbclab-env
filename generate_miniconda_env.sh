e624ca31f3485893c827a61308a4ff03b89c0f71
tsalo <tsalo90@gmail.com>
2015-10-12 11:58:01 -0400

    Update projects and generate environment script.

--- generate_miniconda_env.sh
+++ generate_miniconda_env.sh
@@ -1,8 +1,10 @@
 module add miniconda/2.7
-conda create -p /home/data/nbc/adhd/adhd/.env/python_environment pip numpy scipy sympy
-source activate /home/data/nbc/adhd/adhd/.env/python_environment
+conda create -p /home/data/nbc/adhd/adhd/envs/ADHD pip numpy scipy sympy funcsigs
+source activate /home/data/nbc/adhd/adhd/envs/ADHD
+conda install cython ipython ipython-notebook matplotlib pandas numexpr scikit-learn tornado accelerate dateutil
 pip install nibabel
 pip install https://github.com/nipy/nipy/archive/master.zip
 pip install https://github.com/nipy/nipype/archive/master.zip
-conda install cython ipython ipython-notebook gcc matplotlib pandas numexpr scikit-learn tornado accelerate dateutil
 pip install https://github.com/moloney/dcmstack/archive/master.zip
+conda remove --features mkl
+conda env export > /home/data/nbc/adhd/adhd/envs/environment.yml
