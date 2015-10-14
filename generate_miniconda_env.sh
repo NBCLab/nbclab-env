fa57569246fd4dde92ff3225e65dddc1380af78a
tsalo <tsalo90@gmail.com>
2015-10-14 10:58:30 -0400

    Update generate_miniconda_env.sh

--- generate_miniconda_env.sh
+++ generate_miniconda_env.sh
@@ -1,10 +1,9 @@
 module add miniconda/2.7
-conda create -p /home/data/nbc/adhd/adhd/envs/ADHD pip numpy scipy sympy funcsigs
+conda create -p /home/data/nbc/adhd/adhd/envs/ADHD pip
 source activate /home/data/nbc/adhd/adhd/envs/ADHD
-conda install cython ipython ipython-notebook matplotlib pandas numexpr scikit-learn tornado accelerate dateutil
+conda install numpy scipy sympy funcsigs cython ipython ipython-notebook matplotlib pandas numexpr scikit-learn tornado accelerate dateutil
 pip install nibabel
 pip install https://github.com/nipy/nipy/archive/master.zip
 pip install https://github.com/nipy/nipype/archive/master.zip
 pip install https://github.com/moloney/dcmstack/archive/master.zip
-conda remove --features mkl
-conda env export > /home/data/nbc/adhd/adhd/envs/environment.yml
+#conda remove --features mkl
