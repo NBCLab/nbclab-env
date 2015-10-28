a15956600417807e3ec9477d2c0f707699639839
tsalo <tsalo90@gmail.com>
2015-10-28 10:23:03 -0400

    Fixed to match convention (env instead of envs).

--- generate_miniconda_env.sh
+++ generate_miniconda_env.sh
@@ -1,6 +1,6 @@
 module add miniconda/2.7
-conda create -p /home/data/nbc/adhd/adhd/envs/ADHD pip
-source activate /home/data/nbc/adhd/adhd/envs/ADHD
+conda create -p /home/data/nbc/adhd/adhd/env/ADHD pip
+source activate /home/data/nbc/adhd/adhd/env/ADHD
 conda install numpy scipy sympy funcsigs cython ipython ipython-notebook matplotlib pandas numexpr scikit-learn tornado accelerate dateutil
 pip install nibabel
 pip install https://github.com/nipy/nipy/archive/master.zip
