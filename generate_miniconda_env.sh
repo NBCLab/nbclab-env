ec597529c708657b0e8a6018bf67efe830a63a4b
Taylor Salo <tsalo006@fiu.edu>
2015-12-11 00:33:03 -0500

    Update generate_miniconda_env.sh

--- generate_miniconda_env.sh
+++ generate_miniconda_env.sh
@@ -1,9 +1,9 @@
 module add miniconda/2.7
-conda create -p /home/data/nbc/adhd/adhd/env pip
-source activate /home/data/nbc/adhd/adhd/env
-conda install numpy scipy sympy funcsigs cython ipython ipython-notebook matplotlib pandas numexpr scikit-learn tornado accelerate dateutil
+conda create -p ~/nbclab-env/env pip
+source activate ~/nbclab-env/env
+conda install numpy libgfortran scipy sympy funcsigs cython ipython ipython-notebook matplotlib pandas numexpr scikit-learn tornado accelerate dateutil
 pip install nibabel
 pip install https://github.com/nipy/nipy/archive/master.zip
 pip install https://github.com/nipy/nipype/archive/master.zip
 pip install https://github.com/moloney/dcmstack/archive/master.zip
-conda list > /home/data/nbc/adhd/adhd/requirements.txt
+conda list > ~/nbclab-env/requirements.txt
