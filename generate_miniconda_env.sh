3ed686990b008f2d5282cb5ee6f85bde5b51470f
tsalo <tsalo90@gmail.com>
2015-12-10 23:56:21 -0500

    Update generate_miniconda_env.sh and bash_environment

--- generate_miniconda_env.sh
+++ generate_miniconda_env.sh
@@ -1,8 +1,9 @@
 module add miniconda/2.7
-conda create -p /home/data/nbc/adhd/adhd/env/ADHD pip
-source activate /home/data/nbc/adhd/adhd/env/ADHD
+conda create -p /home/data/nbc/adhd/adhd/env pip
+source activate /home/data/nbc/adhd/adhd/env
 conda install numpy scipy sympy funcsigs cython ipython ipython-notebook matplotlib pandas numexpr scikit-learn tornado accelerate dateutil
 pip install nibabel
 pip install https://github.com/nipy/nipy/archive/master.zip
 pip install https://github.com/nipy/nipype/archive/master.zip
 pip install https://github.com/moloney/dcmstack/archive/master.zip
+conda list > /home/data/nbc/adhd/adhd/requirements.txt
